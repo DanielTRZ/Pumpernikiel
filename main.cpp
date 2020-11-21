7d2fbbdbda09fc4bf3ee703341a8918c930004e8
DanielTRZ <71105883+DanielTRZ@users.noreply.github.com>
2020-11-21 12:23:31 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    string haslo;
+    cout<<"Podaj slowo"<<endl;
+
+    while(cin>>haslo)
+    {
+
+        if(haslo != "pumpernikiel")
+        {
+
+
+            cout<< "Podaj slowo: ";
+        }
+
+        else
+        {
+            cout<< "Udalo ci sie opuscic petle"<<endl;
+            break;
+        }
+    }
+
+    return 0;
+}
